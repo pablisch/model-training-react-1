7d54c49cfebfc3f8cfd19b6cd05cce0bfa8aa67d
Pablo <pablito@me.com>
2025-04-23 19:37:11 +0100

    chore: display regression line for up to 8 iterations on the graph.

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -9,33 +9,72 @@ import {
   Tooltip,
   Legend,
 } from 'recharts'
-import { InputData } from '../../types/types.ts'
+import { InputData, TrainingData } from '../../types/types.ts'
+import helpers from '../../util/helpers.ts'
 
-// type DataPoint = { feature: number; label: number }
+const arbitraryAxisExtension = 1.1
+const colors = ['#dd7f03', '#203e9f', '#7b1d9f', '#158b1f', '#dd1435']
 
 type Props = {
   inputData: InputData
+  trainingData: TrainingData
   weight: number
   bias: number
 }
 
-export const Graph: React.FC<Props> = ({ inputData, weight, bias }) => {
+export const Graph: React.FC<Props> = ({
+  inputData,
+  trainingData,
+  weight,
+  bias,
+}) => {
   const data = inputData.data
   // Extract feature and label values
   const features = data.map(d => d.feature)
   const labels = data.map(d => d.label)
 
   // X-axis and Y-axis increments
-  const xAxisIncrement = 1
-  const yAxisIncrement = 5
+
+  console.log('****()** inc 24:', helpers.findGraphIncrement(24))
+  console.log('****()** inc 25:', helpers.findGraphIncrement(25))
+  console.log('****()** inc 327:', helpers.findGraphIncrement(327))
 
   // Calculate max values for x and y
-  const maxFeature = Math.max(...features)
-  const maxLabel = Math.max(...labels)
+  const maxFeature = Math.max(...features) * arbitraryAxisExtension
+  console.log('****()** maxFeature:', maxFeature)
+  const maxLabel = Math.max(...labels) * arbitraryAxisExtension
+  console.log('****()** maxLabel:', maxLabel)
+
+  console.log('****()** inc X:', helpers.findGraphIncrement(maxFeature))
+  console.log('****()** inc Y:', helpers.findGraphIncrement(maxLabel))
+
+  const xAxisIncrement = helpers.findGraphIncrement(maxFeature * 1.3)
+  const yAxisIncrement = helpers.findGraphIncrement(maxLabel * 1.3)
 
   // Calculate the rounded limits for axes
   const roundedMaxX = Math.ceil(maxFeature) + xAxisIncrement
   const roundedMaxY = Math.ceil(maxLabel / yAxisIncrement) * yAxisIncrement
+  console.log('****()** roundedMaxX:', roundedMaxX)
+  console.log('****()** roundedMaxY:', roundedMaxY)
+
+  // Find selected iterations
+  const selectedIterations = trainingData.filter(
+    iteration => iteration.selected
+  )
+  console.log('****()** selectedIterations:', selectedIterations)
+
+  const regressionLines = []
+  for (const iteration of selectedIterations) {
+    const regressionLine = [
+      { feature: 0, label: 0 + iteration.bias },
+      {
+        feature: roundedMaxX,
+        label: iteration.weight * roundedMaxX + iteration.bias,
+      },
+      {iteration: iteration.iteration},
+    ]
+    regressionLines.push(regressionLine)
+  }
 
   // Calculate regression line based on the range of the graph
   const minFeature = 0
@@ -44,6 +83,7 @@ export const Graph: React.FC<Props> = ({ inputData, weight, bias }) => {
     { feature: minFeature, label: weight * minFeature + bias },
     { feature: maxFeatureForLine, label: weight * maxFeatureForLine + bias },
   ]
+  console.log('****()** regressionLine:', regressionLine)
 
   return (
     <ResponsiveContainer width="100%" height={400}>
@@ -56,10 +96,16 @@ export const Graph: React.FC<Props> = ({ inputData, weight, bias }) => {
           dataKey="feature"
           name="Feature"
           domain={[0, roundedMaxX]}
+          label={{
+            value: inputData.featureLabel,
+            position: 'insideBottom',
+            offset: -5,
+            style: { fontWeight: 'bold', fill: '#333' },
+          }}
           ticks={Array.from(
             { length: roundedMaxX + 1 },
             (_, i) => i * xAxisIncrement
-          )} // Fixed increments for X axis
+          )}
         />
 
         {/* Y Axis with fixed increments */}
@@ -68,25 +114,35 @@ export const Graph: React.FC<Props> = ({ inputData, weight, bias }) => {
           dataKey="label"
           name="Label"
           domain={[0, roundedMaxY]}
+          label={{
+            value: inputData.labelLabel,
+            angle: -90,
+            position: 'insideLeft',
+            offset: 10,
+          }}
           ticks={Array.from(
             { length: roundedMaxY / yAxisIncrement + 1 },
             (_, i) => i * yAxisIncrement
-          )} // Fixed increments for Y axis
+          )}
         />
 
         <Tooltip />
-        <Legend />
+        <Legend verticalAlign="top" align="center" />
 
         {/* Data points */}
         <Scatter name="Data points" data={data} fill="#8884d8" />
 
         {/* Regression line */}
-        <Scatter
-          name="Regression Line"
-          data={regressionLine}
-          line
-          fill="#ff7300"
-        />
+        {regressionLines.map((line, index) => (
+          <Scatter
+            key={index}
+            name={`Iteration ${line[2].iteration}`}
+            data={line}
+            line
+            fill={colors[index % colors.length]} // cycle through colors
+          />
+        ))}
+      
       </ScatterChart>
     </ResponsiveContainer>
   )
